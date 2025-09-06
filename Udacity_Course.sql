67d7f3bed3e7cbd033aabafe5540ef0b8d4e04af
Kevin <97813966+kchiang8@users.noreply.github.com>
2025-09-06 14:05:36 -0700

    Update Udacity_Course.sql

--- Udacity_Course.sql
+++ Udacity_Course.sql
@@ -1,1 +1,8 @@
--- comments
+/*
+Basic SQL Notes:
+SELECT indicates which column(s) you want to be given the data for.
+FROM specifies from which table(s) you want to select the columns. Notice the columns need to exist in this table. 
+LIMIT command is always the very last part of a query, use when only want to see first couple of rows of dataset. 
+
+
+*/
