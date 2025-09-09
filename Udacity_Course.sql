78987e1e2f093bdf57ba87d1ef30da1b6fa94146
Kevin <97813966+kchiang8@users.noreply.github.com>
2025-09-08 23:15:43 -0700

    Update Udacity_Course.sql

--- Udacity_Course.sql
+++ Udacity_Course.sql
@@ -3,6 +3,6 @@ Basic SQL Notes:
 SELECT indicates which column(s) you want to be given the data for.
 FROM specifies from which table(s) you want to select the columns. Notice the columns need to exist in this table. 
 LIMIT command is always the very last part of a query, use when only want to see first couple of rows of dataset. 
-
+ORDER BY is used afte SELECT & FROMM but before LIMIT. Is like a filter in excel/sheets and only gets sorted during query.
 
 */
